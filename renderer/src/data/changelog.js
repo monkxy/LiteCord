b34f26a65a73b862446b75adb438b94f238f4959
Barcode <88603393+Actuallybarcode@users.noreply.github.com>
2023-01-28 16:49:40 -0600

    Add files via upload

---
+++ renderer/src/data/changelog.js
@@ -0,0 +1,13 @@
+// fixed, improved, added, progress
+export default {
+    description: "More features are coming soon!",
+    changes: [
+        {
+            title: "Bug Fixes",
+            type: "fixed",
+            items: [
+                "Fixed crashing on right click.",
+            ]
+        }
+    ]
+};
---
+++ renderer/src/data/data.js
@@ -0,0 +1,3 @@
+export {default as Config} from "./config";
+export {default as SettingsConfig} from "./settings";
+export {default as Changelog} from "./changelog";
